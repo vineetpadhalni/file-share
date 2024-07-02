7d7b4a2a54541bf549f55a86e60726104760a203
Vineet Padhalni <145456749+vineetpadhalni@users.noreply.github.com>
2024-07-02 16:37:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,39 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
 function connectDB() {
-    // Database connection 🥳
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+    // Database connection
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false // Set to false, as `true` is deprecated
+    });
+
     const connection = mongoose.connection;
-    connection.once('open', () => {
+
+    connection.on('connected', () => {
         console.log('Database connected 🥳🥳🥳🥳');
-    }).catch(err => {
-        console.log('Connection failed ☹️☹️☹️☹️');
+    });
+
+    connection.on('error', (err) => {
+        console.error('Connection failed ☹️☹️☹️☹️', err); // Log the error
+    });
+
+    // No need to catch here, since once('open') doesn't return a promise with catch
+
+    connection.once('open', () => {
+        console.log('Connection established'); // This is executed when connection is open
+    });
+
+    connection.once('disconnected', () => {
+        console.log('Database disconnected');
+    });
+
+    process.on('SIGINT', async () => {
+        await mongoose.connection.close();
+        process.exit(0);
     });
 }
-
-// mIAY0a6u1ByJsWWZ
 
 module.exports = connectDB;
